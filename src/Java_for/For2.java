3cc8bc10942a2ed0df60072fa6d43da68388dc15
Estar0622 <judaikr@gmail.com>
2024-07-28 02:09:08 +0900

    Hello New World

---
+++ src/Java_for/For2.java
@@ -0,0 +1,9 @@
+package Java_for;
+
+public class For2 {
+    public static void main (String arge[]) {
+        for(int i =1, j = 10; i<=10; i++, j--) {
+            System.out.println("i="+i+",j="+j);
+        }
+    }
+}
