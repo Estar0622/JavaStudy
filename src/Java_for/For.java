6876a0bf6601d39bd40bdbe16236c452b458117d
Estar0622 <judaikr@gmail.com>
2024-07-28 02:08:50 +0900

    Hello New World

---
+++ src/Java_for/For.java
@@ -0,0 +1,9 @@
+package Java_for;
+
+public class For {
+    public static void main(String args[]) {
+        for (int i = 1; i <=10; i=i+2) { //i = 1,3,5,7,9 & { } 안에 조건식을 5번 반복
+            System.out.println("강해린 이상하다");
+        }
+    }
+}
