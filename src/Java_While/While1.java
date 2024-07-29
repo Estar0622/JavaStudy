a5645c5dfb5ad2ec3517de6654ec8c5edf753963
Estar0622 <judaikr@gmail.com>
2024-07-30 03:21:10 +0900

    Hello New World

---
+++ src/Java_While/While1.java
@@ -0,0 +1,11 @@
+package Java_While;
+
+public class While1 {
+    public static void main(String arge[]){
+        int i = 5; //반복할 횟수를 넣는다.
+
+        while (i--!=0){
+            System.out.println("강해린 이상하다");
+        }
+    }
+}
